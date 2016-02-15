20c7368938d7592efe8c31defc147ba61ae99c86
Reid Beels <mail@reidbeels.com>
2016-02-15 14:50:54 +0100

    Copy in Shrine Storage interface

--- lib/shrine/storage/you_tube.rb
+++ lib/shrine/storage/you_tube.rb
@@ -3,7 +3,41 @@ require "shrine/storage/you_tube/version"
 module Shrine
   module Storage
     module YouTube
-      # Your code goes here...
+      def initialize(*args)
+        # initializing logic
+      end
+
+      def upload(io, id, metadata = {})
+        # uploads `io` to the location `id`
+      end
+
+      def download(id)
+        # downloads the file from the storage
+      end
+
+      def open(id)
+        # returns the remote file as an IO-like object
+      end
+
+      def read(id)
+        # returns the file contents as a string
+      end
+
+      def exists?(id)
+        # checks if the file exists on the storage
+      end
+
+      def delete(id)
+        # deletes the file from the storage
+      end
+
+      def url(id, options = {})
+        # URL to the remote file, accepts options for customizing the URL
+      end
+
+      def clear!(confirm = nil)
+        # deletes all the files in the storage
+      end
     end
   end
 end
